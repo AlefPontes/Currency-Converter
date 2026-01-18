82f0a2365969c86d80ee7ac64b6bfb0287a52eaa
Alef de Pontes <alef.pandrade@gmail.com>
2026-01-18 17:29:17 -0300

    bug: corrigido  tratamento de erro e refatoração do código

---
+++ js/error.js
@@ -0,0 +1,19 @@
+export class AppError extends Error {
+  constructor(message, code) {
+    super(message);
+    this.code = code;
+  }
+}
+
+export class ApiError extends AppError {
+  constructor(status, message = "Erro da API") {
+    super(message, "API_ERROR");
+    this.status = status;
+  }
+}
+
+export class InternetError extends Error {
+  constructor() {
+    super("Erro de Internet", "INTERNET_ERROR");
+  }
+}
